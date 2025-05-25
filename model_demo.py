4e78de05748529e41d15f7179d4bb11f3effbba3
ktrin-u <ktrinidad2@up.edu.ph>
2025-05-25 22:47:57 +0800

    feat: add test results for cnn

--- model_demo.py
+++ model_demo.py
@@ -1,27 +1,40 @@
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # to suppresss the tensorflow messages
+from pathlib import Path
 
 import keras
 import tensorflow as tf
 from PIL import Image
-from pathlib import Path
 
+classes = [
+    "Banded_Chlorosis",
+    "Brown_Rust",
+    "Brown_Spot",
+    "Viral",
+    "Yellow_Leaf",
+    "Healthy",
+]
+classes.sort()
+# print(classes)
 
-MODEL_PATH = "hf://ktrin-u/clearflow-drain-classifier"
+MODEL_PATH = "hf://ktrin-u/KEY-cnn"
 TEST_PATH = Path("./test/")
 OUTPUT_FILENAME = Path("./results.csv")
 
 model = keras.models.load_model(MODEL_PATH)
-output_file = open(OUTPUT_FILENAME, "xt")
+output_file = open(OUTPUT_FILENAME, "w+")
+image_names = os.listdir(TEST_PATH)
+image_names.sort(key=lambda name: int(name.split(".")[0]))
 
-for img_name in os.listdir(TEST_PATH):
+for img_name in image_names:
     img = Image.open(TEST_PATH.joinpath(img_name))
     img_array = keras.utils.img_to_array(img)
     img_resized = keras.layers.Resizing(128, 128, pad_to_aspect_ratio=True)(img_array)
     img_resized = tf.expand_dims(img_resized, axis=0)
-    prediction = model.predict(img_resized, batch_size=1, verbose="0")  # type: ignore
-    result = f"{img_name},{prediction}"
-    print(result)
+    prediction = model.predict(img_resized, batch_size=1, verbose="3")  # type: ignore
+    prediction_class_index = prediction.argmax()
+    # print(prediction)
+    result = f"{img_name},{classes[prediction_class_index]}"
+    # print(result)
     output_file.write(result + "\n")
 
 output_file.close()
