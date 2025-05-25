612ce0dc618cbf998801130a02a5458b023917a9
ktrin-u <ktrinidad2@up.edu.ph>
2025-05-25 17:47:07 +0800

    style: add type annotation

--- web_app/web/views.py
+++ web_app/web/views.py
@@ -1,9 +1,8 @@
 # Create your views here.
-
 from django.http import HttpResponse
 from django.template import loader
 
 
-def home(request):
+def home(request) -> HttpResponse:
     template = loader.get_template("qme.html")
     return HttpResponse(template.render(None, request))
