53c652d50c845bd81382aa439faeb4dc5bd2196f
EdzoAcyatan <csacyatan1@up.edu.ph>
2025-05-27 00:32:11 +0800

    feat: Added alternative predict functions to include an integer variable for how many images that will be tested

--- model_demo.py
+++ model_demo.py
@@ -21,14 +21,19 @@ classes.sort()
 TEST_PATH = Path("./test/")
 
 def method1_predict():
+    # Model path and output filename to be used for the first method
     MODEL_PATH = "hf://ktrin-u/KEY-cnn"
     OUTPUT_FILENAME = Path("./method1-results.csv")
 
+    # This loads the CNN model and prepares the output file to be written to
     model = keras.models.load_model(MODEL_PATH)
     output_file = open(OUTPUT_FILENAME, "w+")
     image_names = os.listdir(TEST_PATH)
     image_names.sort(key=lambda name: int(name.split(".")[0]))
 
+    # We then go through each image, open it, process it to be suitable for the model, then get the predicted class
+    # The result is a combination of the image name and its predicted class. 
+    # Each result is written into the output file followed by a new line
     for img_name in image_names:
         img = Image.open(TEST_PATH.joinpath(img_name))
         img_array = keras.utils.img_to_array(img)
@@ -41,18 +46,27 @@ def method1_predict():
         print(result)
         output_file.write(result + "\n")
 
+    #Once all the images have been given predictions, the output file is closed 
     output_file.close()
-    
-def method1_predict(num):
+
+# A second function almost identical to the first with the variable 'n'
+# n is the number of images that will be used for predictions.
+def method1_predict(n: int):
+    # Model path and output filename to be used for the first method
     MODEL_PATH = "hf://ktrin-u/KEY-cnn"
     OUTPUT_FILENAME = Path("./method1-results.csv")
 
+    # This loads the CNN model and prepares the output file to be written to
     model = keras.models.load_model(MODEL_PATH)
     output_file = open(OUTPUT_FILENAME, "w+")
     image_names = os.listdir(TEST_PATH)
     image_names.sort(key=lambda name: int(name.split(".")[0]))
-    image_names = image_names[:num]
+    # The first n images will be used instead of all of them
+    image_names = image_names[:n]
 
+    # We then go through each image, open it, process it to be suitable for the model, then get the predicted class
+    # The result is a combination of the image name and its predicted class. 
+    # Each result is written into the output file followed by a new line
     for img_name in image_names:
         img = Image.open(TEST_PATH.joinpath(img_name))
         img_array = keras.utils.img_to_array(img)
@@ -65,21 +79,28 @@ def method1_predict(num):
         print(result)
         output_file.write(result + "\n")
 
+    #Once all the images have been given predictions, the output file is closed 
     output_file.close()
 
 def method2_predict():
+    # Model path and output filename to be used for the first method
     MODEL_PATH = "hf://ktrin-u/KEY-ViT"
     OUTPUT_FILENAME = Path("./method2-results.csv")
 
+    # This loads the ViT model and prepares the output file to be written to
     model = keras.models.load_model(MODEL_PATH)
     output_file = open(OUTPUT_FILENAME, "w+")
     image_names = os.listdir(TEST_PATH)
     image_names.sort(key=lambda name: int(name.split(".")[0]))
 
+    # We then go through each image, open it, process it to be suitable for the model, then get the predicted class
+    # The result is a combination of the image name and its predicted class. 
+    # Each result is written into the output file followed by a new line
     for img_name in image_names:
         img = Image.open(TEST_PATH.joinpath(img_name))
         img_array = keras.utils.img_to_array(img)
-        img_resized = tf.expand_dims(img_array, axis=0)
+        img_resized = keras.layers.Resizing(96, 96, pad_to_aspect_ratio=True)(img_array)
+        img_resized = tf.expand_dims(img_resized, axis=0)
         prediction = model.predict(img_resized, batch_size=1, verbose="2")  # type: ignore
         prediction_class_index = prediction.argmax()
         # print(prediction)
@@ -87,6 +108,41 @@ def method2_predict():
         print(result)
         output_file.write(result + "\n")
 
+    #Once all the images have been given predictions, the output file is closed 
+    output_file.close()
+
+# A second function almost identical to the first with the variable 'n'
+# n is the number of images that will be used for predictions.
+def method2_predict(n: int):
+    # Model path and output filename to be used for the first method
+    MODEL_PATH = "hf://ktrin-u/KEY-ViT"
+    OUTPUT_FILENAME = Path("./method2-results.csv")
+
+    # This loads the ViT model and prepares the output file to be written to
+    model = keras.models.load_model(MODEL_PATH)
+    output_file = open(OUTPUT_FILENAME, "w+")
+    image_names = os.listdir(TEST_PATH)
+    image_names.sort(key=lambda name: int(name.split(".")[0]))
+    # The first n images will be used instead of all of them
+    image_names = image_names[:n]
+
+    # We then go through each image, open it, process it to be suitable for the model, then get the predicted class
+    # The result is a combination of the image name and its predicted class. 
+    # Each result is written into the output file followed by a new line
+    for img_name in image_names:
+        img = Image.open(TEST_PATH.joinpath(img_name))
+        img_array = keras.utils.img_to_array(img)
+        img_resized = keras.layers.Resizing(96, 96, pad_to_aspect_ratio=True)(img_array)
+        img_resized = tf.expand_dims(img_resized, axis=0)
+        prediction = model.predict(img_resized, batch_size=1, verbose="2")  # type: ignore
+        prediction_class_index = prediction.argmax()
+        # print(prediction)
+        result = f"{img_name},{classes[prediction_class_index]}"
+        print(result)
+        output_file.write(result + "\n")
+
+    #Once all the images have been given predictions, the output file is closed 
     output_file.close()
 
 method1_predict()
+#method2_predict()
