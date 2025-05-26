8260e32dc440fedcc7f1540e228bdac5930f6b02
EdzoAcyatan <csacyatan1@up.edu.ph>
2025-05-27 00:32:07 +0800

    feat: Added alternative predict functions to include an integer variable for how many images that will be tested

--- model_demo.py
+++ model_demo.py
@@ -5,6 +5,7 @@ import keras
 import tensorflow as tf
 from PIL import Image
 
+#Upon inspecting the given data, there are 6 classes given
 classes = [
     "Banded_Chlorosis",
     "Brown_Rust",
@@ -16,17 +17,41 @@ classes = [
 classes.sort()
 # print(classes)
 
+# Path to the folder that contains the test imagegs
 TEST_PATH = Path("./test/")
 
 def method1_predict():
     MODEL_PATH = "hf://ktrin-u/KEY-cnn"
-    TEST_PATH = Path("./test/")
     OUTPUT_FILENAME = Path("./method1-results.csv")
 
     model = keras.models.load_model(MODEL_PATH)
     output_file = open(OUTPUT_FILENAME, "w+")
     image_names = os.listdir(TEST_PATH)
     image_names.sort(key=lambda name: int(name.split(".")[0]))
+
+    for img_name in image_names:
+        img = Image.open(TEST_PATH.joinpath(img_name))
+        img_array = keras.utils.img_to_array(img)
+        img_resized = keras.layers.Resizing(96, 96, pad_to_aspect_ratio=True)(img_array)
+        img_resized = tf.expand_dims(img_resized, axis=0)
+        prediction = model.predict(img_resized, batch_size=1, verbose="2")  # type: ignore
+        prediction_class_index = prediction.argmax()
+        # print(prediction)
+        result = f"{img_name},{classes[prediction_class_index]}"
+        print(result)
+        output_file.write(result + "\n")
+
+    output_file.close()
+    
+def method1_predict(num):
+    MODEL_PATH = "hf://ktrin-u/KEY-cnn"
+    OUTPUT_FILENAME = Path("./method1-results.csv")
+
+    model = keras.models.load_model(MODEL_PATH)
+    output_file = open(OUTPUT_FILENAME, "w+")
+    image_names = os.listdir(TEST_PATH)
+    image_names.sort(key=lambda name: int(name.split(".")[0]))
+    image_names = image_names[:num]
 
     for img_name in image_names:
         img = Image.open(TEST_PATH.joinpath(img_name))
