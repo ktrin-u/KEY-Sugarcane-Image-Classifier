5396720f3e7ac2afedc298861fd67bf86461ca96
ktrin-u <ktrinidad2@up.edu.ph>
2025-05-27 20:43:23 +0800

    feat: add web app v1.1.0

--- web_app/web/api.py
+++ web_app/web/api.py
@@ -7,9 +7,10 @@ from pathlib import Path
 import keras
 import tensorflow as tf
 from django.http import JsonResponse
-from ninja import NinjaAPI, Swagger
+from ninja import NinjaAPI, Swagger, Schema
 from ninja.files import UploadedFile
 from PIL import Image
+from typing import Literal
 
 classes = [
     "Banded_Chlorosis",
@@ -28,7 +29,11 @@ api = NinjaAPI(title="Sugarcane Image Classifier", description="This allows you 
 method1 = keras.models.load_model(METHOD1_PATH)
 method2 = keras.models.load_model(METHOD2_PATH)
 
-@api.post("/predict/method1", tags=["predict"])
+class PredictPathRequest(Schema):
+    path: Path  # Use Absolute path
+    image_count: int  # Set to 0 or a negative number for ALL
+
+@api.post("/predict/method1", tags=["method1"], summary="Predict multiple uploaded images using CNN.")
 def predict_using_CNN(request, images: list[UploadedFile]) -> JsonResponse:
     results: dict[str, str] = {}
     for image in images:
@@ -41,15 +46,59 @@ def predict_using_CNN(request, images: list[UploadedFile]) -> JsonResponse:
         results[str(image.name)] = classes[prediction_class_index]
     return JsonResponse(results)
 
-@api.post("/predict/method2", tags=["predict"])
+@api.post("/predict/method1/all", tags=["method1"], summary="Predict multiple images in a given path using CNN.")
+def predict_using_CNN_path(request, data: PredictPathRequest) -> JsonResponse:
+    PATH = Path(data.path)
+    image_names = os.listdir(PATH)
+    image_names.sort(key=lambda name: int(name.split(".")[0]))
+    results: dict[str, str] = {}
+    for img_name in image_names[:data.image_count if data.image_count > 0 else None:]:
+        # Use a try-except block to filter out non-images
+        try:
+            # Open the Image using PIL
+            img = Image.open(PATH.joinpath(img_name))
+        except Exception:
+            print(f"{img_name} could not be opened by PIL.")
+            continue  # go to next iteration
+        img_array = keras.utils.img_to_array(img)
+        img_resized = keras.layers.Resizing(96, 96, pad_to_aspect_ratio=True)(img_array)
+        img_resized = tf.expand_dims(img_resized, axis=0)
+        prediction = method1.predict(img_resized, batch_size=1, verbose="0")  # type: ignore ;
+        prediction_class_index = prediction.argmax()
+        results[str(img_name)] = classes[prediction_class_index]
+    return JsonResponse(results)
+
+@api.post("/predict/method2", tags=["method2"], summary="Predict multiple uploaded images using ViT.")
 def predict_using_ViT(request, images: list[UploadedFile]) -> JsonResponse:
     results: dict[str, str] = {}
     for image in images:
         img = Image.open(image.file)
         img_array = keras.utils.img_to_array(img)
-        img_resized = keras.layers.Resizing(96, 96, pad_to_aspect_ratio=True)(img_array)
+        img_resized = keras.layers.Resizing(224, 224, pad_to_aspect_ratio=True)(img_array)
         img_resized = tf.expand_dims(img_resized, axis=0)
         prediction = method2.predict(img_resized, batch_size=1, verbose="0")  # type: ignore
         prediction_class_index = prediction.argmax()
         results[str(image.name)] = classes[prediction_class_index]
+    return JsonResponse(results)
+
+@api.post("/predict/method2/all", tags=["method2"], summary="Predict multiple images in a given path using ViT.")
+def predict_using_ViT_path(request, data: PredictPathRequest) -> JsonResponse:
+    PATH = Path(data.path)
+    image_names = os.listdir(PATH)
+    image_names.sort(key=lambda name: int(name.split(".")[0]))
+    results: dict[str, str] = {}
+    for img_name in image_names[:data.image_count if data.image_count > 0 else None:]:
+        # Use a try-except block to filter out non-images
+        try:
+            # Open the Image using PIL
+            img = Image.open(PATH.joinpath(img_name))
+        except Exception:
+            print(f"{img_name} could not be opened by PIL.")
+            continue  # go to next iteration
+        img_array = keras.utils.img_to_array(img)
+        img_resized = keras.layers.Resizing(224,224, pad_to_aspect_ratio=True)(img_array)
+        img_resized = tf.expand_dims(img_resized, axis=0)
+        prediction = method2.predict(img_resized, batch_size=1, verbose="0")  # type: ignore ;
+        prediction_class_index = prediction.argmax()
+        results[str(img_name)] = classes[prediction_class_index]
     return JsonResponse(results)
